0142f488b6d37e1cefd5f4c75ff1c2734c8e49f4
Kendallyn <Kendallynlyon@hotmail.com>
2017-12-02 14:04:51 -0800

    checking off and delete task functionality working

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var state = {
     tasks: [{
         name: 'Dust',
         checked: false,
+        description: 'Dust all the furniture'
     }, {
         name: 'Organize',
         checked: true,
+        description: 'Organize all bedrooms closets'
     }]
 };
 
@@ -16,21 +18,21 @@ function addTask(state, task) {
     return state.tasks;
 }
 
-//check the task
-function checkTask(state, taskName) {
+//checkoff the task
+function checkTask(state, tasksName) {
     for (var i = 0; i < state.tasks.length; i++) {
-        if (state.tasks[i].name === taskName) {
+        if (state.tasks[i].name === tasksName) {
             state.tasks[i].checked = !state.tasks[i].checked;
         }
     }
 }
 
 //delete the task
-function deleteTask(state, taskName) {
+function deleteTask(state, tasksName) {
     var tasksArray = state.tasks;
     var index;
     for (var i = 0; i < tasksArray.length; i++) {
-        if (tasksArray[i].name === taskName) {
+        if (tasksArray[i].name === tasksName) {
             index = i;
         }
     }
@@ -44,9 +46,9 @@ function renderList(state) {
     $.each(state.tasks, function (taskKey, taskValue) {
         buildTheHtmlOutput += '<li>';
         if (taskValue.checked == false) {
-            buildTheHtmlOutput += '<span class="to-do-list-task">' + taskValue.name + '</span>';
+            buildTheHtmlOutput += '<span class="to-do-list-task">' + taskValue.name + ": " + taskValue.description + '</span>';
         } else {
-            buildTheHtmlOutput += '<span class="to-do-list-task-checked">' + taskValue.name + '</span>';
+            buildTheHtmlOutput += '<span class="to-do-list-task to-do-list-task-checked">' + taskValue.name + ": " + taskValue.description + '</span>';
         }
         buildTheHtmlOutput += '<div class="taskControls">';
         buildTheHtmlOutput += '<button class="taskToggle">';
@@ -68,15 +70,31 @@ $(document).ready(function () {
     $("#to-do-list-form").on('submit keypress', function (event) {
         if (event.type === 'submit') {
             event.preventDefault();
-            var taskName = $('#to-do-list-entry').val();
+            var tasksName = $('#to-do-list-entry').val();
             var taskItem = {
-                name: taskName,
+                name: tasksName,
                 checked: false,
+                description: tasksDescription
             }
-            if (taskName) {
+            if (tasksName) {
                 addTask(state, taskItem);
                 renderList(state);
             }
         }
     });
+});
+
+//toggle checked or unchecked
+$('ul').on('click', 'button.taskToggle', function (event) {
+    var tasksName = $(this).closest('li').find('.to-do-list-task').text();
+    checkTask(state, tasksName);
+    renderList(state);
+    console.log('made it');
+});
+
+//delete this item from the list
+$('ul').on('click', 'button.taskDelete', function (event) {
+    var tasksName = $(this).closest('li').find('.to-do-list-task').text();
+    deleteTask(state, tasksName);
+    renderList(state);
 });
